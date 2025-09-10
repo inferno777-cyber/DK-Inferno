dc8b873d447b51fad676418c8d4b30ff4963147c
inferno777-cyber <dkinferno999@gmail.com>
2025-09-10 17:02:46 +0630

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,4 +1,5 @@
 let count=document.getElementById("countcon");
+
 let audio=new Audio("https://inferno777-cyber.github.io/audio-demo1/bubble-pop-04-323580.mp3");
 
 count.addEventListener("click",function(){
@@ -6,4 +7,13 @@ count.addEventListener("click",function(){
     setTimeout(()=>{
     window.location.href="firststep.html";
     },500);
-})
+});
+
+let birthday=document.getElementById("birthdaycon");
+
+birthday.addEventListener("click",function(){
+    audio.play();
+    setTimeout(()=>{
+        window.location.href="birthday.html";
+    },500);
+});
